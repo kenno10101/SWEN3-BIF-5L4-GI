b335ad37836fe282684632b848bec0fee14378d4
Kenn-Michael Sanga <kennsanga@gmail.com>
2025-10-28 16:34:16 +0100

    response

--- SWEN_DMS/SWEN_DMS.PL/app.js
+++ SWEN_DMS/SWEN_DMS.PL/app.js
@@ -6,13 +6,11 @@ document.getElementById("loadDocuments").addEventListener("click", async () => {
         const response = await fetch("/api/Document/e988b34f-0f09-4d0e-a1ee-23ce2784c1c4");
         if (!response.ok) throw new Error(`Server error: ${response.status}`);
 
-        const docs = await response.json();
+        const doc = await response.json();
         list.innerHTML = "";
-        docs.forEach(doc => {
-            const li = document.createElement("li");
-            li.textContent = `${doc.FileName} (${doc.Id})`;
-            list.appendChild(li);
-        });
+        const li = document.createElement("li");
+        li.textContent = `${doc.fileName} (${doc.id})`;
+        list.appendChild(li);
     } catch (err) {
         list.innerHTML = `<li style="color:red;">${err.message}</li>`;
     }
