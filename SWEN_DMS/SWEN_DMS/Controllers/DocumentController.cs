004616d6b3a9e498facd5fff353a6d3562dd8d62
Dicle <dicle.becer@hotmail.com>
2025-11-13 13:13:38 +0100

    publish OCR request on upload; add empty worker

--- SWEN_DMS/SWEN_DMS/Controllers/DocumentController.cs
+++ SWEN_DMS/SWEN_DMS/Controllers/DocumentController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using SWEN_DMS.BLL.Services;
+using SWEN_DMS.BLL.Interfaces;
 using SWEN_DMS.DTOs;
+using SWEN_DMS.DTOs.Messages;
+
 
 namespace SWEN_DMS.Controllers;
 
@@ -9,12 +12,14 @@ namespace SWEN_DMS.Controllers;
 public class DocumentController : ControllerBase
 {
     private readonly DocumentService _service;
+    private readonly IMessagePublisher _publisher;   // rabbitmq
 
-    public DocumentController(DocumentService service)
+    public DocumentController(DocumentService service, IMessagePublisher publisher) // rabbitmq
     {
         _service = service;
+        _publisher = publisher;            // rabbitmq
     }
-
+    
     // all documents
     [HttpGet]
     public async Task<ActionResult<IEnumerable<DocumentDto>>> GetAll()
@@ -47,7 +52,16 @@ public class DocumentController : ControllerBase
         }
 
         var created = await _service.AddDocumentAsync(dto, filePath);
-
+        
+        // send MQ-message (empty OCR worker gets it)
+        var msg = new OcrRequestMessage
+        {
+            DocumentId    = created.Id,                              
+            FileName      = created.FileName,
+            UploadedAtUtc = created.UploadedAt.ToUniversalTime()     
+        };
+        await _publisher.PublishAsync(msg);
+        
         return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
     }
     
